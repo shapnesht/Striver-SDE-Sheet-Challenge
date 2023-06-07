c2f63008eecf5372ca332ea1cbbccf3d217b3d0f
shapnesh singh tiwari <shapnesht@gmail.com>
2023-06-07 23:54:26 +0530

    problem 8

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Problem 3 :- next permutation
 Problem 4 :- max Subarray Sum
 Problem 5 :- sort 0,1,2
 Problem 6 :- stock - buy & sell
+
+problem 8 :- merge intervals
---
+++ mergeIntervals.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+{
+    vector<vector<int>> ans(n, vector<int>(m));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+
+        }
+    }
+}
