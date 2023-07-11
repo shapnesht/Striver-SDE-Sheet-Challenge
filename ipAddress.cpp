73f1d33bf097b79535cb83262596336f760d2b21
shapnesh singh tiwari <shapnesht@gmail.com>
2023-07-11 23:31:36 +0530

    problem 110

--- ipAddress.cpp
+++ ipAddress.cpp
@@ -1,67 +1,32 @@
 #include <bits/stdc++.h>
-using namespace std;
-
 int compareVersions(string a, string b)
 {
     int n = a.size();
     int m = b.size();
 
-    int i = 0;
-    vector<long> s1, s2;
-    while (i < n)
+    int i = 0, j = 0;
+
+    while (i < n || j < m)
     {
-        long cur = 0;
-        while (i < n && a[i] >= '0' && a[i] <= '9')
+        long cur1 = 0, cur2 = 0;
+        while (i < n && a[i] != '.')
         {
-            cur *= 10;
-            cur += (a[i] - '0');
+            cur1 *= 10;
+            cur1 += (a[i] - '0');
             i++;
         }
-        s1.push_back(cur);
-        i++;
-    }
-    i = 0;
-    while (i < m)
-    {
-        long cur = 0;
-        while (i < m && b[i] >= '0' && b[i] <= '9')
+        while (j < m && b[j] != '.')
         {
-            cur *= 10;
-            cur += (b[i] - '0');
-            i++;
+            cur2 *= 10;
+            cur2 += (b[j] - '0');
+            j++;
         }
-        s2.push_back(cur);
-        i++;
-    }
-    int len = max(s1.size(), s2.size());
-    for (i = 0; i < len; i++)
-    {
-        if (i >= s1.size())
-        {
-            while (i < len)
-            {
-                if (s2[i] != 0)
-                    return -1;
-                i++;
-            }
-            return 0;
-        }
-        if (i >= s2.size())
-        {
-            while (i < len)
-            {
-                if (s1[i] != 0)
-                    return 1;
-                i++;
-            }
-            return 0;
-        }
-        if (s1[i] == s2[i])
-            continue;
-        if (s1[i] < s2[i])
+        if (cur1 < cur2)
             return -1;
-        else
+        if (cur1 > cur2)
             return 1;
+        i++;
+        j++;
     }
     return 0;
 }
